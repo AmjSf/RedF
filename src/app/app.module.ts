1fa701cfce74b292267d5a00666f9cccddae0de6
AmjSf <amj.soufiane@gmail.com>
2023-01-16 14:07:53 +0100

    add wip second timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CountDownComponent } from './count-down/count-down.component';
-
+import { PrezComponent } from './prez/prez.component';
+const routes: Routes = [
+  { path: 'prez', component: PrezComponent },
+  { path: '', component: CountDownComponent },
+]
 @NgModule({
   declarations: [
     AppComponent,
-    CountDownComponent
+    CountDownComponent,
+    PrezComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/prez/prez.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prez',
+  templateUrl: './prez.component.html',
+  styleUrls: ['./prez.component.css']
+})
+export class PrezComponent {
+
+}
